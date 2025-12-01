d97d8dc79776914f0916feae5636c12dd515ced4
Ludvig Norin <luddenorin@gmail.com>
2025-12-01 21:10:23 +0100

    added comments

--- cmake/toolchains/raspberryPi.cmake
+++ cmake/toolchains/raspberryPi.cmake
@@ -1,6 +1,7 @@
 #CMAKE_SYSTEM_NAME is default set to the same value as the CMAKE_HOST_SYSTEM_NAME.
 #For CrossCompilation we need to explicitly tell which os we are targeting in this case Linux(Linux value * all linux distros
 set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
 #Set the compilers for the targeted environments stored on my computer at..
 set(CMAKE_C_COMPILER )
