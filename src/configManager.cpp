230eae29e3273ed318a54a29e727525cbf569f0d
Ludvig Norin <luddenorin@gmail.com>
2025-12-05 14:09:58 +0100

    added constructor and extract methods to parse and construct object with config data

--- src/configManager.cpp
+++ src/configManager.cpp
@@ -1,3 +1,72 @@
-//
-// Created by Ludvi on 12/4/2025.
-//
+#include "configManager.hpp"
+#include <iostream>
+#include <fstream>
+
+//constructor object reads json objects and stores data for each attribute in configManager
+
+configManager::configManager(std::string& configFilePath) {
+    //define configfile stream that will be used, set the parameters on what exceptions will be catchable on failure
+    //try to open the file can throw runtime_error if failed **Covers open failure and read failure**?
+    std::ifstream configFile;
+    configFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+        configFile.open(configFilePath);
+    } catch (std::ifstream::failure& e) {
+        throw std::runtime_error("Can't open config file: " + std::string(configFilePath));
+    }
+    //try to parse the filestream and store the parsed data as a json object. catch exception and throw runtime_error
+    //if failed to read.
+    json config;
+    try {
+        config = json::parse(configFile);
+    }
+    catch (std::exception& e) {
+        throw std::runtime_error("Parse error at byte: " + std::string(e.what()) + "\n");
+    }
+
+    //extract the data for each attribute declared inside this class and initialise each attribute with that data.
+    extract_modbusTCP(config);
+    extract_modbusRTU(config);
+    extract_mqttPub(config);
+}
+
+//load following objects with its needed configs.
+void configManager::setMqttPubConfig(mqttPub &object) {
+
+}
+void configManager::setModbusRtuConfig(modbusRTU &object) {
+
+}
+void configManager::setModbusTcpConfig(modbusTCP &object) {
+
+}
+
+
+
+
+
+//private extract methods to load each attribute with its corresponding configdata
+void configManager::extract_modbusTCP(json &config) {
+    if(config.contains("MODBUS_TCP")){
+        this->modbusTcpConf = config["MODBUS_TCP"];
+    }
+    else {
+        throw std::runtime_error("Modbus TCP config not found");
+    }
+}
+void configManager::extract_modbusRTU(json &config) {
+    if (config.contains("MODBUS_RTU")) {
+        this->modbusRtuConf = config["MODBUS_RTU"];
+    }
+    else {
+        throw std::runtime_error("Modbus RTU config not found");
+    }
+}
+void configManager::extract_mqttPub(json &config){
+    if (config.contains("MQTT_PUB")) {
+        this->mqttPubConf = config["MQTT_PUB"];
+    }
+    else {
+        throw std::runtime_error("MQTT PUB config not found");
+    }
+}
--- src/include/configManager.hpp
+++ src/include/configManager.hpp
@@ -1,8 +1,11 @@
 
 #ifndef CONFIGMANAGER_HPP
 #define CONFIGMANAGER_HPP
-#include <iostream>
+
 #include <nlohmann/json.hpp>
+#include "mqttPub.hpp"
+#include "modbusRTU.hpp"
+#include "modbusTCP.hpp"
 
 using namespace nlohmann;
 
@@ -12,16 +15,21 @@ class configManager {
         json modbusRtuConf;
         json modbusTcpConf;
         json mqttPubConf;
+        //private methods to extract configs into attribute objects of this class.
+        void extract_modbusTCP(json &config);
+        void extract_modbusRTU(json &config);
+        void extract_mqttPub(json &config);
 
     public:
     //Constructor will read a json object from a config file and initialise the config attributes list above.
-    configManager();
-    ~configManager();
+        explicit configManager(std::string& configFilePath);
 
-    //each setter will take a object as argument and set the object with its required configs.
-    void setModbusRtuConfig(modbusRtu& object);
-    void setModbusTcpConfig(modbusTcp& object);
-    void setModbusPubConfig(mqttPub& object);
+        //~configManager();
+
+    //each setter will take an object as argument and set the object with its required configs.
+        void setModbusRtuConfig(modbusRTU& object);
+        void setModbusTcpConfig(modbusTCP& object);
+        void setMqttPubConfig(mqttPub& object);
 
 };
 
