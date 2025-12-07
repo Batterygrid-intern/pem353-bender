d3827ca5c30eb3a14f8c8880dee6368fcd585810
Ludvig Norin <luddenorin@gmail.com>
2025-12-07 12:55:20 +0100

    refactored and made the modbusRTU constructor take  configmanager object ass argument and load the modbusRTU class with its needed settings

--- src/modbusRTU.cpp
+++ src/modbusRTU.cpp
@@ -1,7 +1,7 @@
 #include "modbusRTU.hpp"
 
-
-modbusRTU::modbusRTU(configManager& configs) {
-    configManager()
-
+//constructor will be passed with the config manager, config manager will initialize the config settings that the
+//modbusRTU object will need to connect and to read over the rs-485 line
+modbusRTU::modbusRTU(configManager& config) {
+    this->settings = config.getModbusRtuConfig();
 }
