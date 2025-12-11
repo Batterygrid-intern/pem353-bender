be97476316f076f9f3d182b98af29454179b6051
Ludvig Norin <luddenorin@gmail.com>
2025-12-11 12:38:14 +0100

    implemented server loop(not finished)

--- src/modbusTCP.cpp
+++ src/modbusTCP.cpp
@@ -1,4 +1,9 @@
 #include "modbusTCP.hpp"
+#include <stdexcept>
+#include <unistd.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include <iostream>
 
 //constructor initializes settings
 modbusTCP::modbusTCP(const mbTcpSettings &settings) : settings_(settings) {
@@ -14,7 +19,7 @@ modbusTCP::modbusTCP(const mbTcpSettings &settings) : settings_(settings) {
     }
 }
 
-//start the thread
+//if worker is not already running server application on thread run it
 void modbusTCP::start() {
     if (running_) return;
     running_ = true;
@@ -36,6 +41,68 @@ void modbusTCP::stop() {
     }
 }
 
+//multiclient server loop.
+void modbusTCP::run() {
+    //listen for connection on created socket
+    listen();
+    //create master_set (controls the initial file descriptor?
+    fd_set master_set;
+    fd_set read_set;
+    FD_ZERO(&master_set);
+    FD_SET(server_socket_, &master_set);
+    //holds the largest file descriptor number
+    int fdmax = server_socket_;
+
+    while (running_) {
+        //copy master_set;(original to read_set which will be modifed.
+        //reads_set keeps control on file descritpors.??
+        read_set = master_set;
+
+        if (select(fdmax + 1, &read_set, NULL, NULL, NULL) == -1) {
+            if (!running_) break;
+            throw std::runtime_error("modbusTCP: select error: " + std::string(strerror(errno)));
+            continue;
+        }
+
+        for (int s = 0; s < fdmax; s++) {
+            if (!FD_ISSET(s, &read_set)) {
+                continue;
+            }
+            //new client connection
+            if (s == server_socket_) {
+                sockaddr_in clientaddr{};
+                socklen_t addr_len = sizeof(clientaddr);
+                int newfd = accept(server_socket_, reinterpret_cast<sockaddr *>(&clientaddr), &addr_len);
+                if (newfd == -1) {
+                    throw std::runtime_error("modbusTCP: accept error: " + std::string(strerror(errno)));
+                    continue;
+                }
+                FD_SET(newfd, &read_set);
+                if (newfd > fdmax) {
+                    fdmax = newfd;
+                }
+                std::cout << "New client connected: socket " << newfd << std::endl;
+            } else {
+                modbus_set_socket(ctx_, s);
+                uint8_t query[MODBUS_TCP_MAX_ADU_LENGHT];
+                int rc = modbus_receive(ctx_, query);
+                if (rc > 0) {
+                    modbus_reply(ctx_, query, rc, mb_mapping_);
+                } else {
+                    std::cout << "Client disconnected: socket " << s << "\n";
+                    close(s);
+                    FD_CLR(s, &master_set);
+                }
+            }
+        }
+    }
+    for (int s = 0; s <= fdmax; s++) {
+        if (FD_ISSET(s, &master_set)) {
+            close(s);
+        }
+    }
+}
+
 
 //listen for connection
 void modbusTCP::listen() {
@@ -47,7 +114,7 @@ void modbusTCP::listen() {
 
 
 //Destructor that frees upp memory of modbus struct ptrs
-//stop() the server runing on the thread
+//close serial port connection and free allocated memory for context ptrs
 modbusTCP::~modbusTCP() {
     stop();
     if (mb_mapping_ != nullptr) {
@@ -57,9 +124,5 @@ modbusTCP::~modbusTCP() {
         modbus_close(ctx_);
         modbus_free(ctx_);
         ctx_ = nullptr;
-    }
-    if (server_socket_ >= 0) {
-        close(server_socket_);
-        server_socket_ = -1;
     }
 }
